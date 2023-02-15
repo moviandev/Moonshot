9eecb3e05eecb74fbf7c765ed1348f178c7b860c
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-14 23:18:42 -0300

    rollback

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,35 +7,10 @@
 
 import SwiftUI
 
-struct User: Codable {
-    let name: String
-    let address: Address
-}
-
-struct Address: Codable {
-    let street: String
-    let city: String
-}
-
 struct ContentView: View {
     var body: some View {
-        Button("Decode JSON") {
-            let input = """
-            {
-                "name": "Matheus Viana",
-                "address": {
-                    "street": "50, Matheus Viana Avenue",
-                    "city": "Sao Paulo"
-                }
-            }
-            """
-            
-            let data = Data(input.utf8)
-            
-            if let user = try? JSONDecoder().decode(User.self, from: data) {
-                print(user.address.street)
-            }
-        }
+        Text("Hello, world!")
+            .padding()
     }
 }
 
