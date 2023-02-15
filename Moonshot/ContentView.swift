94361bba472c9a39398daca12ede14420b88238e
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-14 23:31:59 -0300

    adding items into a grid, and we can say if the size is fixed or adaptive with a maximum and a minimum

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -8,9 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    let layout = [
+        GridItem(.adaptive(minimum: 80, maximum: 120))
+    ]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView {
+            LazyVGrid(columns: layout) {
+                ForEach(0..<1000) {
+                    Text("Item \($0)")
+                }
+            }
+        }
     }
 }
 
