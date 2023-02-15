5e4e4671cd166cdabcae3bb66ad9f8186f95c59f
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-14 22:33:54 -0300

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Matheus Viana on 14/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
