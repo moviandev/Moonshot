fc787297f1bad1ed03e151735364c5904cc2bf5d
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-14 22:59:28 -0300

    There's a catch when using ScrollView it'll create all the view inside of it not on demand, to make our code more efficient we should use LazyVStack or LazyHStack

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,12 +7,25 @@
 
 import SwiftUI
 
+struct CustomText: View {
+    let text: String
+    
+    var body: some View {
+        Text(text)
+    }
+    
+    init(_ text: String) {
+        print("Creating a new CustomText")
+        self.text = text
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         ScrollView {
-            VStack(spacing: 10) {
+            LazyVStack(spacing: 10) {
                 ForEach(0..<100) {
-                    Text("Line \($0)")
+                    CustomText("Line \($0)")
                         .font(.title)
                 }
             }
