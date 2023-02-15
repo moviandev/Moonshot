62eae758735c6fee4c3653a9f64c0c6ee7ddca82
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-14 23:34:03 -0300

    Rollback

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -8,17 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
-    let layout = [
-        GridItem(.adaptive(minimum: 80, maximum: 120))
-    ]
     var body: some View {
-        ScrollView(.horizontal) {
-            LazyHGrid(rows: layout) {
-                ForEach(0..<1000) {
-                    Text("Item \($0)")
-                }
-            }
-        }
+        Text("Hello, world!")
+            .padding()
     }
 }
 
