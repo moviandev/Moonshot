a69cac6a36b476f9475624b5edf83eb511c97ca9
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-15 23:14:18 -0300

    Add images, and create astronaut struct to decode from json file and created extension to bundle to decode our file

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Matheus Viana on 15/02/23.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
