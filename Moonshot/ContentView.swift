c47b88842ad044d519803257dcaf8fe9e0f746e8
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-15 23:28:25 -0300

    using generics to make possible to decode all json file of our app

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -8,11 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    let astronauts = Bundle.main.decode("astronauts.json")
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    
+    let missions: [Mission] = Bundle.main.decode("missions.json")
     
     var body: some View {
-        Text("\(astronauts.count)")
-            .padding()
+        VStack{
+            Text("\(astronauts.count)")
+                .padding()
+            Text("\(missions.count)")
+                .padding()
+        }
     }
 }
 
