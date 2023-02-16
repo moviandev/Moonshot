48ce1adb4f6755db14b54af96d9f0ff08303070c
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-15 23:51:31 -0300

    Create Color-Theme with 2 new colors for our theme

---
+++ Moonshot/Color-Theme.swift
@@ -0,0 +1,19 @@
+//
+//  Color-Theme.swift
+//  Moonshot
+//
+//  Created by Matheus Viana on 15/02/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension ShapeStyle where Self == Color {
+    static var darkBackground: Color {
+        Color(red: 0.1, green: 0.1, blue: 0.2)
+    }
+    
+    static var lightBackground: Color {
+        Color(red: 0.2, green: 0.2, blue: 0.2)
+    }
+}
