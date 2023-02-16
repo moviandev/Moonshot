5eec2eb86d78fe89aed250b32a63c2339858e5c9
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-15 23:38:56 -0300

    create a navigationView with a scrollView and a lazyGrid to show all the missions

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,15 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
-    
     let missions: [Mission] = Bundle.main.decode("missions.json")
     
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
     var body: some View {
-        VStack{
-            Text("\(astronauts.count)")
-                .padding()
-            Text("\(missions.count)")
-                .padding()
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            Text("Detail view")
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                    
+                                    Text(mission.launchDate ?? "N/A")
+                                        .font(.headline)
+                                }
+                                .frame(maxWidth: .infinity)
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Moonshot")
         }
     }
 }
