4f72527be153d4dd6c8138af8d59e26c9a43ace6
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-15 23:21:14 -0300

    Create mission struct to decode from json

---
+++ Moonshot/Mission.swift
@@ -0,0 +1,20 @@
+//
+//  Mission.swift
+//  Moonshot
+//
+//  Created by Matheus Viana on 15/02/23.
+//
+
+import Foundation
+
+struct Mission: Codable, Identifiable {
+    struct CrewRole: Codable {
+        let name: String
+        let role: String
+    }
+
+    let id: Int
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
