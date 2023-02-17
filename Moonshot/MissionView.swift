c0119bac1680841441056862f20f6f02703ac867
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-16 22:23:57 -0300

    Create missionView

---
+++ Moonshot/MissionView.swift
@@ -0,0 +1,20 @@
+//
+//  MissionView.swift
+//  Moonshot
+//
+//  Created by Matheus Viana on 16/02/23.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MissionView_Previews: PreviewProvider {
+    static var previews: some View {
+        MissionView()
+    }
+}
