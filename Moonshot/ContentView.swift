ebe001dd606f280639e28a50e320e4a2b2116eb7
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-17 23:54:18 -0300

    Create gridView

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -19,41 +19,8 @@ struct ContentView: View {
     
     var body: some View {
         NavigationView {
-            ScrollView {
-                LazyVGrid(columns: columns) {
-                    ForEach(missions) { mission in
-                        NavigationLink {
-                            MissionView(mission: mission, astronauts: astronauts)
-                        } label: {
-                            VStack {
-                                Image(mission.image)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 100, height: 100)
-                                    .padding()
-                                
-                                VStack {
-                                    Text(mission.displayName)
-                                        .font(.headline)
-                                        .foregroundColor(.white)
-                                    
-                                    Text(mission.formattedLaunchDate)
-                                        .font(.headline)
-                                        .foregroundColor(.white.opacity(0.5))
-                                }
-                                .padding(.vertical)
-                                .frame(maxWidth: .infinity)
-                                .background(.lightBackground)
-                            }
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(.lightBackground)
-                            )
-                            .padding([.horizontal, .bottom])
-                        }
-                    }
-                }
+            Group {
+                GridView(astronauts: astronauts, missions: missions)
             }
             .navigationTitle("Moonshot")
             .background(.darkBackground)
